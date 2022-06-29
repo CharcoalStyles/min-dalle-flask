c5356a76c15eb176aeb6272db0d90a56e1990b24
Aaron Styles <aaron@charcoalstyles.com>
2022-06-29 12:00:07 +1000

    APi is now serving an image

--- api.py
+++ api.py
@@ -1,11 +1,19 @@
 import argparse
+from io import BytesIO
 import os
 from PIL import Image
 
 from min_dalle.generate_image import generate_image_from_text
 
-from flask import Flask
+from flask import Flask, send_file
 app = Flask(__name__)
+
+
+def serve_pil_image(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'png')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
 
 @app.route('/')
 def hello_world():
@@ -23,13 +31,7 @@ def get_image():
       # image_token_count = args.image_token_count
     )
     
-    return "ok"
+    return serve_pil_image(image)
 
 if __name__ == '__main__':
     app.run()
-
-def serve_pil_image(pil_img):
-    img_io = StringIO()
-    pil_img.save(img_io, 'png')
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/png')
